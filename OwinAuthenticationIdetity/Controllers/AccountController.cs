9bc9d5898e5ecc1365d974b669cf6d9b564e307f
sonujose <sonujose993@gmail.com>
2018-04-01 12:34:38 +0530

    Updating controllers

---
+++ OwinAuthenticationIdetity/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OwinAuthenticationIdetity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserManager 
+            => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+
+        public SignInManager<IdentityUser, string> SignInManager
+            => HttpContext.GetOwinContext().Get<SignInManager<IdentityUser, string>>();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(RegisterModel model)
+        {
+            var signInStatus =  await SignInManager.PasswordSignInAsync(model.userName, model.password, true, true);
+
+            if(signInStatus.Equals(SignInStatus.Success))
+            {
+                return RedirectToAction("Index", "home");
+            } else
+            {
+                ModelState.AddModelError("", "Invalid Credentials");
+                return View(model);
+            }            
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            var identityResult = await UserManager.CreateAsync(new IdentityUser(model.userName), model.password);
+            if(identityResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
+            return View(model);
+        }
+
+
+    }
+
+    public class RegisterModel
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ OwinAuthenticationIdetity/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Configuration;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(OwinAuthenticationIdetity.Startup))]
+
+namespace OwinAuthenticationIdetity
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //string connectionString = ConfigurationManager.AppSettings["DefaultConnection"];
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Database=PlotoMark.AspNetIdentity";
+            app.CreatePerOwinContext(() => new IdentityDbContext(connectionString));
+
+            app.CreatePerOwinContext<UserStore<IdentityUser>>(
+                (opt, cont) => new UserStore<IdentityUser>(cont.Get<IdentityDbContext>()));
+
+            app.CreatePerOwinContext<UserManager<IdentityUser>>(
+                (opt, cont) => new UserManager<IdentityUser>(cont.Get<UserStore<IdentityUser>>()));
+
+            app.CreatePerOwinContext<SignInManager<IdentityUser, string>>(
+                (opt, cont) => new SignInManager<IdentityUser, string>(cont.Get<UserManager<IdentityUser>>(), cont.Authentication));
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+
+
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
